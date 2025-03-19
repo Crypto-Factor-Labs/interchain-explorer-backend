7e60bd8a787967036220f8f8a5aeced6bb86d6c3
dAppCaP <cf_github@appcap.nl>
2025-03-19 09:28:16 +0100

    fix: Use DB_NAME instead of DB_DATABASE
    Use the correct environment variable `DB_NAME`.
    Using the non-existing variable `DB_DATABASE` results in migration in the default postgres database.

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,7 +13,7 @@ export const DATA_SOURCE_OPTIONS: DataSourceOptions = {
   port: parseInt(process.env["DB_PORT"] || "5432"),
   username: process.env["DB_USERNAME"]!,
   password: process.env["DB_PASSWORD"]!,
-  database: process.env["DB_DATABASE"]!,
+  database: process.env["DB_NAME"]!,
   synchronize: false,
   logging: false,
   entities: [IndexerLock, MasterChainBlockEntity, PartialChainBlockEntity],
