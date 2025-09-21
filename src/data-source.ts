ef816c27624504cfb375cb3c70b0b77e586f08a3
ShadowsCrypto <shadows@crypto-factor.io>
2025-09-22 00:21:48 +0300

    cleanup

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,8 +6,25 @@ import { TransactionEntity } from "./storage/entities/transaction.entity.js";
 import { ExecutionPartEntity } from "./storage/entities/execution-part.entity.js";
 import { ChainEventEntity } from "./storage/entities/chain-event.entity.js";
 import { CfrPriceHistoryEntity } from "./storage/entities/cfr-price-history.entity.js";
-import { join } from 'path';
 import * as dotenv from "dotenv";
+import {CreateInterchainSchema_1710765072069} from "./storage/migration/1710765072069-create-interchain-schema.js";
+import {CreateCfrPriceHistoryTable_1710765072070} from "./storage/migration/1710765072070-cfr-price-history.js";
+import {
+    CreateTransactionsAndExecutionParts1710765072071
+} from "./storage/migration/1710765072071-create-transactions-and-execution-parts.js";
+import {
+    MasterBlockHeightsAndTimestamps1710765072072
+} from "./storage/migration/1710765072072-masterblock-heights-and-timestamps.js";
+import {
+    PartialBlockHeightsAndTimestamps1710765072073
+} from "./storage/migration/1710765072073-partialblock-heights-and-timestamps.js";
+import {
+    ExecPartsAndTxHashUniqueIndexes1710765072074
+} from "./storage/migration/1710765072074-execparts-and-txhash-unique-indexes.js";
+import {CreateChainEvents1710765072075} from "./storage/migration/1710765072075-create-chain-events.js";
+import {
+    ReplaceTxHashesWithEventHashes1710765072076
+} from "./storage/migration/1710765072076-replace-tx-hashes-with-event-hashes.js";
 dotenv.config();
 
 export const DATA_SOURCE_OPTIONS: DataSourceOptions = {
@@ -32,6 +49,14 @@ export const DATA_SOURCE_OPTIONS: DataSourceOptions = {
   ],
   // Execute the migrations in lexicograpic order of the filenames in dist/storage/migration/
   migrations: [
-    join(process.cwd(), 'dist', 'storage', 'migration', '*.js')],
+    CreateInterchainSchema_1710765072069,
+      CreateCfrPriceHistoryTable_1710765072070,
+      CreateTransactionsAndExecutionParts1710765072071,
+      MasterBlockHeightsAndTimestamps1710765072072,
+      PartialBlockHeightsAndTimestamps1710765072073,
+      ExecPartsAndTxHashUniqueIndexes1710765072074,
+      CreateChainEvents1710765072075,
+      ReplaceTxHashesWithEventHashes1710765072076
+  ],
   migrationsRun: true,
 };
